0a7e878b1d58421c0b78b8d2f53c9d77860e7943
Osama Bajaber <obajaber.94@gmail.com>
2024-05-19 23:47:53 -0400

    Update controller.py

--- switch/controller.py
+++ switch/controller.py
@@ -8,6 +8,7 @@ import grpc
 import bfrt_grpc.bfruntime_pb2 as bfruntime_pb2
 import bfrt_grpc.client as gc
 import socket, struct
+import netcl
 
 #Connect to BF Runtime Server 
 interface = gc.ClientInterface(grpc_addr="localhost:50052", client_id=0,device_id=0) 
@@ -35,40 +36,9 @@ table_dec = bfrt_info.table_get("table_dec")
 # Add by request (digest)
 table_conn = bfrt_info.table_get("table_conn")
 
-# =========================================================
-# table_action2 Declassification
-
-# Define the key
-src_ip_addr = 0x0a000002
-dst_ip_addr = 0x0a000003
-key = table_action2.make_key([gc.KeyTuple('hdr.ipv4.dst_addr',dst_ip_addr),
-                            gc.KeyTuple('hdr.ipv4.src_addr',src_ip_addr)])
-data = table_action2.make_data([gc.DataTuple('priority', 9),
-                              gc.DataTuple('decision', 0),
-                              gc.DataTuple('declassify_label', 0xFE)], 'SwitchIngress.table_action2')
-table_action2.entry_add(target, [key], [data])
+netcl.insert_rules()
 
 
-# ========================================================
-# table_action3 label -> dst_address
-dst_ip_addr = struct.unpack("!L", socket.inet_aton("10.0.0.3"))[0]
-key = table_action3.make_key([gc.KeyTuple('hdr.flowtag.tag_label',0x01, 0x01),
-                            gc.KeyTuple('hdr.ipv4.dst_addr',dst_ip_addr)])
-data = table_action3.make_data([gc.DataTuple('priority', 1),
-                              gc.DataTuple('decision', 2)], 'SwitchIngress.table_action3')
-table_action3.entry_add(target, [key], [data])
-
-# ========================================================
-# table_action4 
-key = table_action4.make_key([gc.KeyTuple('hdr.ipv4.dst_addr',0x0a000003),
-                            gc.KeyTuple('hdr.flowtag_id.tag_id',1)])
-data = table_action4.make_data([gc.DataTuple('priority', 5),
-                              gc.DataTuple('decision', 2)], 'SwitchIngress.table_action4')
-table_action4.entry_add(target, [key], [data])
-
-
-
-# ========================================================
 # table_dec
 key = table_dec.make_key([gc.KeyTuple('ig_md.decision', 0)])
 data = table_dec.make_data([], 'NoAction')
